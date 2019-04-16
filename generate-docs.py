de136018614506dd674a43b025c743e8f75fa66f
nielshulstaert <nielshulstaert@gmail.com>
2019-04-16 16:40:16 +0200

    updated ThermoRawFileParser project

--- generate-docs.py
+++ generate-docs.py
@@ -16,7 +16,7 @@ def url_sub(matches):
     if url.endswith('wiki'):
         return '/projects' + url + '/home.html)'
     elif url.endswith('issues' or 'releases'):
-        sys.stdout.write(matches + '\n')
+        sys.stdout.write('fvffffffff' + matches + '\n')
         return matches.group(0)
     else:
         return '/projects' + url + '.html)'
